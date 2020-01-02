abbd65f3f9758071d9cb97f56136b1222052ac6b
Alberto <Alberto@socialpymesvlc.es>
2020-01-02 18:24:13 +0100

    agregada persistencia de redux

--- src/store.js
+++ src/store.js
@@ -1,7 +1,20 @@
-import {compose, createStore} from 'redux'
+import {compose, createStore,applyMiddleware} from 'redux'
 import reducers from './reducers';
+import {save,load} from 'redux-localstorage-simple'
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducers,composeEnhancers());
+// const store = createStore(reducers,composeEnhancers());
+const createStoreWithMiddleware = applyMiddleware(
+    save(), // Saving done here
+)(createStore);
+const store = createStoreWithMiddleware(
+    reducers,
+    load({
+        preloadedState: {//cargamos un estado inicial para redux en caso de no haber estado en el localStorage
+            Carrito: []
+        }
+    }), // Loading done here
+    composeEnhancers(),
+);
 
 export default store;
